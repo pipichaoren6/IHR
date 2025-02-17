a7508f201bea3a614b3e54ed40290c7e52905248
chenluo <2695577569@qq.com>
2025-02-17 23:47:12 +0800

    添加角色管理页面结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <div class="head-role">
+        <el-button type="primary" @click="handleAdd">添加角色</el-button>
+      </div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column align="center" prop="name" label="角色名称" />
+        <el-table-column align="center" prop="desc" label="启用" />
+        <el-table-column align="center" prop="createdAt" label="描述" />
+        <el-table-column align="center" prop="updatedAt" label="操作" />
+      </el-table>
     </div>
+    <el-row align="middle" type="flex" justify="end" style="height: 50px;">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000"
+        :page-size="20"
+        :current-page="1"
+      />
+    </el-row>
   </div>
 </template>
 <script>
@@ -10,3 +27,8 @@ export default {
   name: 'Role'
 }
 </script>
+<style scoped>
+  .head-role {
+    padding: 10px;
+  }
+</style>
